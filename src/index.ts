494e0d786a5e2951e7425033fd7e170e4c91ba9b
tama <tama@ttk1.net>
2019-12-14 01:30:54 +0900

    微調整

--- src/index.ts
+++ src/index.ts
@@ -11,14 +11,13 @@ window.onload = () => {
   function f(dot: Dot, count: number) {
     if (count == 0) {
       ctx.beginPath();
-      ctx.arc(dot.x * 300 + 100, dot.y * 300 + 100, 0.5, 0, 2 * Math.PI, true);
+      ctx.arc(dot.x * 200 + 150, dot.y * 200 + 150, 0.5, 0, 2 * Math.PI, true);
       ctx.stroke();
     } else {
       dot = dot.scale(0.70710678118);
-      f(dot.rotate(- Math.PI / 4), count - 1);
-      f(dot.rotate(- Math.PI * 3 / 4).translate(1, 1), count - 1);
+      f(dot.rotate(Math.PI / 4), count - 1);
+      f(dot.rotate(Math.PI * 3 / 4).translate(1, 1), count - 1);
     }
   }
-
-  f(new Dot(0.5, 0.5), 15);
+  f(new Dot(0.5, 0.5), 13);
 };
